fc9edf7f1c7e5d3a8293b94f14a8627300ed4a50
Roman Pavlenko <x100up@yandex.ru>
2012-12-20 13:59:14 +0400

    fix

--- server/services/ClusterHadoopService.py
+++ server/services/ClusterHadoopService.py
@@ -9,7 +9,7 @@ class ClusterHadoopService(HadoopService):
     def getState(self, clusterHost):
         #data =  json.loads('{"nodes":{"node":[{"rack":"/default-rack","state":"RUNNING","id":"web345:44826","nodeHostName":"web345","nodeHTTPAddress":"web345:8042","healthStatus":"Healthy","lastHealthUpdate":1355993991777,"healthReport":"","numContainers":0,"usedMemoryMB":0,"availMemoryMB":8192},{"rack":"/default-rack","state":"RUNNING","id":"web346:48369","nodeHostName":"web346","nodeHTTPAddress":"web346:8042","healthStatus":"Healthy","lastHealthUpdate":1355993943481,"healthReport":"","numContainers":0,"usedMemoryMB":0,"availMemoryMB":8192},{"rack":"/default-rack","state":"RUNNING","id":"web347:47280","nodeHostName":"web347","nodeHTTPAddress":"web347:8042","healthStatus":"Healthy","lastHealthUpdate":1355993943044,"healthReport":"","numContainers":0,"usedMemoryMB":0,"availMemoryMB":8192}]}}')
 
-        data = self.loadJSON(clusterHost + '/ws/v1/cluster/nodes')
+        data = self.loadJSON('http://' + clusterHost + '/ws/v1/cluster/nodes')
 
         result = {}
 
