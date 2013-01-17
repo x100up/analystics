33927d66fba44177716a1a99136f7aa191a8425b
x100up <x100up@yandex.ru>
2013-01-17 15:23:12 +0400

    commit

--- server/models/Hive.py
+++ server/models/Hive.py
@@ -7,12 +7,12 @@ class HiveTable(Base):
     hiveTableId = Column(Integer, primary_key=True)
     appId = Column(Integer, ForeignKey('app.appId'),  nullable=False, primary_key=True, autoincrement=False)
     eventCode = Column(String(255), name='keyCode')
-    startFrom = Column(DateTime(), nullable=True),
+    startFrom = Column(Date, nullable=True),
 
 class HiveTablePartition(Base):
     __tablename__ = "hiveTablePartition"
 
     hiveTablePartitionId = Column(Integer, primary_key=True)
     hiveTableId = Column(Integer, ForeignKey('hiveTable.hiveTableId'),  nullable=False, primary_key=True, autoincrement=False)
-    partitionDate = Column(DateTime(), nullable=False),
+    partitionDate = Column(Date, nullable=False),
     isCompact = Boolean()
