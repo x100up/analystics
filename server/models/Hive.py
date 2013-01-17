6bca627c4e0585b6a0ad1284c2f42ebc78746134
x100up <x100up@yandex.ru>
2013-01-17 15:19:12 +0400

    commit

--- server/models/Hive.py
+++ server/models/Hive.py
@@ -3,14 +3,16 @@ from sqlalchemy import Integer, String, Date, DateTime, Boolean, Column, Foreign
 
 class HiveTable(Base):
     __tablename__ = "hiveTable"
+
     hiveTableId = Column(Integer, primary_key=True)
     appId = Column(Integer, ForeignKey('app.appId'),  nullable=False, primary_key=True, autoincrement=False)
     eventCode = Column(String(255), name='keyCode')
-    startFrom = Column(DateTime, nullable=True),
+    startFrom = Column(DateTime(), nullable=True),
 
 class HiveTablePartition(Base):
     __tablename__ = "hiveTablePartition"
+
     hiveTablePartitionId = Column(Integer, primary_key=True)
     hiveTableId = Column(Integer, ForeignKey('hiveTable.hiveTableId'),  nullable=False, primary_key=True, autoincrement=False)
-    partitionDate = Column(DateTime, nullable=False),
+    partitionDate = Column(DateTime(), nullable=False),
     isCompact = Boolean()
