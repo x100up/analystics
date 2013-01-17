9f09b004fd632362a3b36430f23807a84931a758
x100up <x100up@yandex.ru>
2013-01-17 15:03:00 +0400

    fix

--- server/models/Hive.py
+++ server/models/Hive.py
@@ -1,13 +1,13 @@
 from Base import Base
 from sqlalchemy.schema import Column, ForeignKey
-from sqlalchemy.types import Integer, String, Date, Boolean
+from sqlalchemy.types import Integer, String, Date, DateTime, Boolean
 
 class HiveTable(Base):
     __tablename__ = "hiveTable"
     hiveTableId = Column(Integer, primary_key=True)
     appId = Column(Integer, ForeignKey('app.appId'),  nullable=False, primary_key=True, autoincrement=False)
     eventCode = Column(String(255), name='keyCode')
-    startFrom = Column(Date, nullable=True),
+    startFrom = Column(DateTime, nullable=True),
 
 class HiveTablePartition(Base):
     __tablename__ = "hiveTablePartition"
---
+++ sql/10-ChangeHiveMeta2.sql
@@ -0,0 +1,1 @@
+ALTER TABLE hiveTable MODIFY startFrom DATETIME;
